af7c9f55bab95688174932bf2a58e5a7969261d6
lysgut <88504963+elixxi@users.noreply.github.com>
2023-08-30 10:50:22 -0300

    mas cambios

--- prisci/src/componentes/navbar.js
+++ prisci/src/componentes/navbar.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Importar el CSS de Bootstrap
 import '../App.css';
-
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top"> {/* Agrega la clase fixed-top */}
       <a className="navbar-brand" href="#">
+        {/* Contenido de la marca */}
       </a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
@@ -13,7 +13,7 @@ const Navbar = () => {
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav ml-auto">
           <li className="nav-item active">
-            <a className="nav-link" href="#">Inicio </a>
+            <a className="nav-link" href="#">Inicio</a>
           </li>
           <li className="nav-item">
             <a className="nav-link" href="#">Servicios</a>
@@ -26,5 +26,4 @@ const Navbar = () => {
     </nav>
   );
 }
-
 export default Navbar;
